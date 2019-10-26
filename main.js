c00d910f7319a5ce1bf6c618ebea069296c37900
rezan83 <rezan.moh83@gmail.com>
2019-10-27 01:22:17 +0200

    adding saving button and styles

--- main.js
+++ main.js
@@ -1,47 +1,82 @@
-
-const editorsDiv = document.getElementById("editors")
+//getting buttonsDiv and input
+const buttonsDiv = document.getElementById("buttonsDiv")
 const input = document.getElementById("input");
 
+// saved value by In button
+var saved = 0;
 
-const editor = ['C','+','-','*','/',1,2,3,4,5,6,7,8,9, 0,'.','=']
-const editorButtons = editor.map(item => {
-    return(`<button class="editor" id="${item}" value="${item}">${item}</button>`)
+// adding buttons to buttonsDiv
+const buttonNames = ['C','In',7,8,9,'+',4,5,6,'-',1,2,3,'*',0,'.','=','/']
+const buttonHtml = buttonNames.map(item => {
+    return(
+        `<button class="editor" id="${item}" value="${item}">
+            ${item}
+        </button>`
+        );
 })
 
-editorButtons.forEach(editorButton => editorsDiv.innerHTML+=editorButton )
+buttonHtml.forEach(btnHtml => buttonsDiv.innerHTML+=btnHtml )
 
-const allEditors = document.querySelectorAll(".editor");
-console.log(allEditors)
-allEditors.forEach(editor => {
-    editor.addEventListener('click', (e)=> editorHandel(e.target.value))
+// selecting buttons and ading event listener
+const allButtons = document.querySelectorAll(".editor");
+allButtons.forEach(button => {
+    button.addEventListener('click', (e)=> buttonHandel(e.target.value))
 })
 
-function editorHandel(btnValue){
+
+// main click handler function
+function buttonHandel(btnValue){
     if (btnValue === "C") {
         resetValue()
     }else if (btnValue === "**2" || btnValue === "**0.5") {
         specialCalculate(btnValue)
     }else if(btnValue === "="){
         calculate();
+    }else if(btnValue === "In"){
+        saveValue()
     }
     else{
         addValue(btnValue)
     }
 }
 
+//****functions to handel every situation ****/
+
+// the delete C button
 function resetValue(){
     input.value = '';
 }
+
+// main calculation
 function calculate() {
 	input.value = eval(input.value)
 }
 
+// add value to the input
 function addValue(btnValue){
     input.value+=btnValue;
 }
 
-function specialCalculate(btnValue){
-    calculate();
-    addValue(btnValue);
-    calculate();
+// saving a value from input to use it later with 'In' button
+function saveValue(){
+    if(input.value && Number(input.value[input.value.length-1])){
+        saved = eval(input.value);
+    }else{
+        input.value+=saved;
+    } 
 }
+
+// claculation by the square-root and square-power buttons
+function specialCalculate(btnValue){
+    if(input.value){
+        calculate();
+        addValue(btnValue);
+        calculate();
+    } 
+}
+
+//***** removing text node after buttons (html problem!!)
+var power = document.getElementById("square-power");
+power.nextSibling.parentNode.removeChild(power.nextSibling);
+var sqRoot = document.getElementById("square-root");
+sqRoot.nextSibling.parentNode.removeChild(sqRoot.nextSibling);
